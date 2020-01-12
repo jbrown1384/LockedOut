fe364d0307c3d90ed558141808993d015d0f6c54
Jeremy Brown <jbrown@inxitehealth.com>
2020-01-11 21:46:39 -0500

    initial commit of deploy script
    deploy script for dependancies and js/css compiling

---
+++ public/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##
+#  Run this file any time that you need to update the code
+#  on a server to ensure all required commands are completed
+# #
+echo "Install Composer"
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+echo "Composer Installed"
+
+echo "Installing composer packages"
+sudo composer install
+
+echo "Installing NodeJS version"
+sudo apt-get install -y nodejs
+echo "NodeJS installed"
+
+echo "Installing NPM"
+sudo apt-get install npm -y
+
+echo "Installing all NPM packages"
+sudo npm install
+
+echo "Generating App.js/App.css"
+sudo npm run dev
+
+echo "Deployment script complete"
